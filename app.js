32717b25da82e6e54ee5d45b789377a21b860c98
KabeloMakganye <88086981+KabeloMakganye@users.noreply.github.com>
2022-08-15 13:50:49 +0200

    second brajoe2

--- app.js
+++ app.js
@@ -136,9 +136,9 @@ app.get('/fn_add_load/:website',(req,res)=> {
      })
 })
 //create account
-function datas(ab,cd) { // ab is password and cd is user name
+function datas(ab) { // ab is password and cd is user name
     var MD5 = require("crypto-js/md5");
-    for (var x = ab.length + cd.length; x--; x > 0) {
+    for (var x = ab.length; x--; x > 0) {
         ab = MD5(ab).toString();
     }
     return ab;
@@ -234,7 +234,7 @@ app.post('/login',(req,res)=> {
     // res.send("Sign up coming soon")
    db.func("fn_add_login",[
     req.body.email,
-    datas(req.body.password,req.body.name)])
+    datas(req.body.password)])
     .then(rows => {
         if (rows[0].fn_add_new_user == 1) {
             res.send("1") //login sucess
